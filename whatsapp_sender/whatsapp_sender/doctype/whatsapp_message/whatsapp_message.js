c41af01d00145474cd8432ce9c5eeffd10f95bbb
Abdulrhman Khaled <bodyono3@gmail.com>
2025-06-03 13:26:58 +0300

    edit custom button

--- whatsapp_sender/whatsapp_sender/doctype/whatsapp_message/whatsapp_message.js
+++ whatsapp_sender/whatsapp_sender/doctype/whatsapp_message/whatsapp_message.js
@@ -1,17 +1,16 @@
 frappe.ui.form.on("whatsapp message", {
 	refresh: function (frm) {
 
-		frm.add_custom_button(__("click"), function () {
+		frm.add_custom_button(__("Send Test Message"), function () {
 
 			frm.call("msg", {
 				token: frm.doc.token,
 				deviceId: frm.doc.device_id,
 				recipient: frm.doc.to,
 				url: frm.doc.url,
-
 			}).then(r => {
 				frappe.msgprint(r.message);;
 			})
-		}, __("Send Test Message"));
+		});
 	}
 });
