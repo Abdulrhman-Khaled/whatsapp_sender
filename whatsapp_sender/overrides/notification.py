56f9ae78f04d1abd04ad6eff68530fbe7f1331e5
Abdulrhman Khaled <bodyono3@gmail.com>
2025-06-03 14:46:34 +0300

    edit notification logic

--- whatsapp_sender/overrides/notification.py
+++ whatsapp_sender/overrides/notification.py
@@ -17,19 +17,17 @@ class ERPGulfNotification(Notification):
     token = frappe.get_doc('whatsapp message').get('token')
     url =  frappe.get_doc('whatsapp message').get('url')
     deviceId =  frappe.get_doc('whatsapp message').get('device_id')
+    msg = frappe.render_template(self.message, context)
 
-    msg1 = frappe.render_template(self.message, context)
-    recipients = self.get_receiver_list(doc,context) 
-    multiple_numbers=[] 
-    for receipt in recipients:
-      number = receipt
-      multiple_numbers.append(number)
-    add_multiple_numbers_to_url=','.join(multiple_numbers)
+    recipients = self.get_receiver_list(doc,context)
+    multiple_numbers = [str(r).strip() for r in recipients if r]
+    add_multiple_numbers_to_url = ','.join(multiple_numbers) 
+  
     payload = {
         "device": deviceId,
         "phone": add_multiple_numbers_to_url,
         "authKey": token,
-        "message": msg1
+        "message": msg
        }
     try:
         time.sleep(10)
@@ -39,9 +37,9 @@ class ERPGulfNotification(Notification):
             response_json = response.json()
             if "sent" in  response_json and  response_json["sent"] == "true":
             # Log success
-              current_time =now()# for geting current time
-              msg1 = frappe.render_template(self.message, context)
-              frappe.get_doc({"doctype":"whatsapp_sender log","title":"Whatsapp message successfully sent ","message":msg1,"to_number":doc.custom_mobile_phone,"time":current_time }).insert()
+              current_time =now()
+              msg = frappe.render_template(self.message, context)
+              frappe.get_doc({"doctype":"whatsapp_sender log","title":"Whatsapp message successfully sent ","message":msg,"to_number":add_multiple_numbers_to_url,"time":current_time }).insert()
             elif "error" in  response_json:
             # Log error
               frappe.log("WhatsApp API Error: " ,  response_json.get("error"))
@@ -55,9 +53,6 @@ class ERPGulfNotification(Notification):
     except Exception as e:
         frappe.log_error(title='Failed to send notification', message=frappe.get_traceback())  
  
-
-# directly pass the function 
-  # call the  send whatsapp with pdf function and send whatsapp without pdf function and it work with the help of condition 
   def send(self, doc):
 
     context = {"doc":doc, "alert": self, "comments": None}
@@ -80,34 +75,13 @@ class ERPGulfNotification(Notification):
               
                        
   def get_receiver_list(self, doc, context):
-    """return receiver list based on the doc field and role specified"""
     receiver_list = []
-    for recipient in self.recipients:
-            if recipient.condition:
-                if not frappe.safe_eval(recipient.condition, None, context):
-                    continue
-            if recipient.receiver_by_document_field:
-              fields = recipient.receiver_by_document_field.split(",")
-              if len(fields)>1:
-                for d in doc.get(fields[1]):
-                  phone_number = d.get(fields[0])
-                  receiver_list.append(phone_number)
-              
-			# For sending messages to the owner's mobile phone number
-            if recipient.receiver_by_document_field == "owner":
-                    receiver_list += get_user_info([dict(user_name=doc.get("owner"))], "mobile_no")
-                    
-			# For sending messages to the number specified in the receiver field
-            elif recipient.receiver_by_document_field:
-                    receiver_list.append(doc.get(recipient.receiver_by_document_field))
-			# For sending messages to specified role
-            if recipient.receiver_by_role:
-                receiver_list += get_info_based_on_role(recipient.receiver_by_role, "mobile_no")
-            # return receiver_list
-    receiver_list = list(set(receiver_list))
-    # removing none_object from the list
-    final_receiver_list = [item for item in receiver_list if item is not None]
-    return final_receiver_list
+    for recipient in self.custom_receivers_phones:
+        phone = recipient.phone_number
+        if phone:
+            receiver_list.append(phone)
+    receiver_list = list(set(filter(None, receiver_list)))               
+    return receiver_list
 
   
     
